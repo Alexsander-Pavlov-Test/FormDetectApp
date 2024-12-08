41d173bf3074ea3577bfc2ac59a8971562c7394d
Alexsander-Pavlov-Test <soul.pam106@gmail.com>
2024-12-08 23:35:27 +0600

    remove prometheus, caddy, grafana, alertmanager, change .env, add .env.sample

--- config/config.py
+++ config/config.py
@@ -1,8 +1,7 @@
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import BaseModel, ConfigDict, MongoDsn, IPvAnyNetwork
+from pydantic import BaseModel, MongoDsn, IPvAnyNetwork
 from starlette.config import Config
-from celery.schedules import crontab
 
 
 base_dir = Path(__file__).resolve().parent.parent
@@ -10,18 +9,6 @@ log_dir = base_dir.joinpath('logs')
 
 
 config = Config('.env')
-
-
-# class TestDBSettings(BaseModel):
-#     """
-#     Настройки тестовой базы данных
-#     """
-#     _engine: str = config('TEST_DB_ENGINE')
-#     _owner: str = config('TEST_DB_USER')
-#     _password: str = config('TEST_DB_PASSWORD')
-#     _name: str = config('TEST_DB_HOST')
-#     _db_name: str = config('TEST_DB_NAME')
-#     url: str = f'{_engine}://{_owner}:{_password}@{_name}/{_db_name}'
 
 
 class DBSettings(BaseModel):
@@ -35,41 +22,6 @@ class DBSettings(BaseModel):
     url: MongoDsn = f'{_engine}://{_owner}:{_password}@{_name}'
 
 
-class CelerySettings(BaseModel):
-    """
-    Настройки Celery
-    """
-    model_config = ConfigDict(
-        arbitrary_types_allowed=True,
-    )
-    TIMEZONE: str = 'Europe/Moscow'
-    TIMEDELTA_PER_DAY: crontab = crontab(minute=0,
-                                         hour=2,
-                                         day_of_week='*/1',
-                                         day_of_month='*/1',
-                                         month_of_year='*/1',
-                                         )
-    TEST_TIMEDELTA: crontab = crontab(minute='*/1')
-
-
-class RabbitSettings(BaseModel):
-    """
-    Настройки RabbitMQ
-    """
-    RMQ_HOST: str = config('RMQ_HOST')
-    RMQ_PORT: str = config('RMQ_PORT')
-    RMQ_USER: str = config('RABBITMQ_DEFAULT_USER')
-    RMQ_PASSWORD: str = config('RABBITMQ_DEFAULT_PASS')
-    broker_url: str = ('amqp://' +
-                       RMQ_USER +
-                       ':' +
-                       RMQ_PASSWORD +
-                       '@' +
-                       RMQ_HOST +
-                       ':' +
-                       RMQ_PORT)
-
-
 class Settings(BaseSettings):
     """
     Настройки проекта
@@ -78,9 +30,6 @@ class Settings(BaseSettings):
         extra='ignore',
     )
     db: DBSettings = DBSettings()
-    # test_db: TestDBSettings = TestDBSettings()
-    celery: CelerySettings = CelerySettings()
-    rabbit: RabbitSettings = RabbitSettings()
     debug: bool = bool(int(config('DEBUG')))
     API_PREFIX: str = '/api/v1'
     BASE_DIR: Path = base_dir
